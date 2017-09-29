fa9ba0fca96bdf33f5eeb71719001e2c895df5bd
Li Jian <lijian@hantek.com>
2017-09-29 10:22:45 +0800

    可以扫描到设备

--- app/src/main/java/co/lijian/ble_demo/MainActivity.java
+++ app/src/main/java/co/lijian/ble_demo/MainActivity.java
@@ -1,13 +1,106 @@
 package co.lijian.ble_demo;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private boolean mScanning;
+    private Handler mHandler;
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHandler = new Handler(); // 用于延时结束扫描
+
+        checkBle();
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        checkBluetooth();
+        scanLeDevice(true);
     }
+
+    // 检查是否支持蓝牙BLE, 如果不支持，关闭程序。
+    private void checkBle(){
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    // TODO: 将某个应用想要打开蓝牙改为，本应用想要打开蓝牙。
+    // 检查是否打开了蓝牙，如果没打开，请求打开蓝牙。
+    private void  checkBluetooth(){
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    // 开始扫描
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "延时结束，即将停止扫描");
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+            Log.d(TAG, "开始扫描");
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            Log.d(TAG, "停止扫描");
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String name =  device.getName();
+                            Log.d(TAG, "Device Name:" + name);
+                            if (name != null && name.equals("HMSoft")) {
+                                Log.d(TAG, "扫到了指定的设备，即将停止扫描");
+                                mScanning = false;
+                                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                            }
+                        }
+                    });
+                }
+            };
 }
